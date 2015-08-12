6d7ca38aee2736d14c286c567a8065e80b5bf16a
Alisa Colon & Vaughn Dice <student@epicodus.com>
2015-08-12 15:55:38 -0700

    add artist and year instance methods and specs

--- lib/album.rb
+++ lib/album.rb
@@ -1,9 +1,32 @@
 class Album
-  define_method(:initialize) do |title|
+  @@album = []
+  define_method(:initialize) do |artist, title, year|
+    @artist = artist
     @title = title
+    @year = year
+  end
+
+  define_method(:artist) do
+    @artist
   end
 
   define_method(:title) do
     @title
+  end
+
+  define_method(:year) do
+    @year
+  end
+
+  define_method(:save) do
+    @@album.push(self)
+  end
+
+  define_singleton_method(:all) do
+    @@album
+  end
+
+  define_singleton_method(:clear) do
+    @@album.clear()
   end
 end
--- spec/album_spec.rb
+++ spec/album_spec.rb
@@ -2,10 +2,43 @@ require('rspec')
 require('album')
 
 describe(Album) do
+  before() do
+    Album.clear()
+  end
+
   describe('#title') do
     it('returns the title of the album') do
-      test_album = Album.new("The Other Side of Bob Dylan")
+      test_album = Album.new("Bob Dylan", "The Other Side of Bob Dylan", 2007)
       expect(test_album.title()).to(eq("The Other Side of Bob Dylan"))
     end
   end
+
+  describe('#artist') do
+    it('returns the artist of the album') do
+      test_album = Album.new("Bob Dylan", "The Other Side of Bob Dylan", 2007)
+      expect(test_album.artist()).to(eq("Bob Dylan"))
+    end
+  end
+
+  describe('#year') do
+    it('returns the year the album was created') do
+      test_album = Album.new("Bob Dylan", "The Other Side of Bob Dylan", 2007)
+      expect(test_album.year()).to(eq(2007))
+    end
+  end
+
+  describe('#save') do
+    it('saves album to albums array') do
+      test_album = Album.new("Bob Dylan", "Blonde on Blonde", 1966)
+      test_album.save()
+      expect(Album.all()).to(eq([test_album]))
+    end
+  end
+
+  describe('.all') do
+    it('starts out empty') do
+      expect(Album.all()).to(eq([]))
+    end
+  end
+
 end
